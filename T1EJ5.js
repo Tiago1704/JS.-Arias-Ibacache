4eba67543ea8329350da914393eeec3b369d680e
Tiago1704 <36915137+Tiago1704@users.noreply.github.com>
2019-08-25 17:24:54 -0300

    Create T1EJ5.js

---
+++ T1EJ5.js
@@ -0,0 +1,16 @@
+<!DOCTYPE>
+  <html>
+    <title>
+      P&aacute;gina con javascript.
+    </title>
+    <script languaje = "javascript">
+      <!--
+      function cambiarColor(){
+        document.bgColor = "red";
+      }
+      //-->
+      </Script>
+      </head>
+      <body>
+      </body>
+      </html>
