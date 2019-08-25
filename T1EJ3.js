200bb0333685ab5a529ffc46fbcb793001b28efc
Tiago1704 <36915137+Tiago1704@users.noreply.github.com>
2019-08-25 17:23:45 -0300

    Create T1EJ3.js

---
+++ T1EJ3.js
@@ -0,0 +1,9 @@
+      var edad = prompt();
+          edadJub = 70;
+
+            if (edad >= edadJub){
+              alert ('Esta persona se puede jubilar')
+            } else {
+              alert ('Esta persona aún no se puede jubilar, le faltan')
+
+            }
