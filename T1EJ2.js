95b3cab535d76595eb2c2f1702d669091ab76e49
Tiago1704 <36915137+Tiago1704@users.noreply.github.com>
2019-08-25 17:23:14 -0300

    Create T1EJ2.js

---
+++ T1EJ2.js
@@ -0,0 +1,15 @@
+      var n1 = 8;
+          n2 = 15;
+          n3 = 0;
+
+      n3 = (n1+n2)
+      console.log('El valor de la suma es de ', n3)
+
+      n3 = (n1-n2);
+      console.log('El valor de la resta es de ', n3)
+
+      n3 = (n1*n2);
+      console.log('El valor del producto es de ', n3)
+
+      n3 = (n1/n2);
+      console.log('El valor de la division es de ', n3)
